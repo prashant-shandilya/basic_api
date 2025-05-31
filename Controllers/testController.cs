74e45efa6379e82b234fabdf162b7f0473b1b720
Prashant Shandilya <91403042+prashant-shandilya@users.noreply.github.com>
2025-05-31 19:35:15 +0530

    Add files via upload

---
+++ Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("/reply")]
+    public class testController : ControllerBase
+    {
+            [HttpGet]
+            public string reply()
+          {
+            return "Get request working !";
+          }
+
+    }
+}
